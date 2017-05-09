851729ed21fc16d765b9d7e14d812e9db9b6143a
amitoshseu <amitosh_cse_seu@yahoo.com>
2017-05-09 12:09:36 +0600

    Final commit May 9

--- paymentoffline.php
+++ paymentoffline.php
@@ -24,9 +24,16 @@
      .tbltwo{float:right;text-align:left; width:40%; margin-right: 14px; margin-top: 12px;}
      .tbltwo tr td{text-align: justify;padding:5px 10px; }
      .booking{padding-bottom: 50px; padding-top: 50px;}
-     .booking a{width: 160px; margin:5px auto 0; padding:7px 0; text-align: 
+     .booking input{width: 160px; margin:5px auto 0; padding:7px 0; text-align: 
       center; display: block; background:#ff0000;border:1px solid #333; color: #fff; border-radius: 3px; font-size: 25px;}
  </style>
+
+
+<form id="payment_gw" name="payment_gw" method="POST" action="https://sandbox.sslcommerz.com/gwprocess/v3/process.php">
+  
+
+
+
 
  <div class="main">
     <div class="content">
@@ -65,10 +72,25 @@
                 <td><?php 
                 $total = $result['rate']* $result['tryTrip'] * $result['vehicleNumber']; 
                 echo $total;?></td>
+
+                <input type="hidden" name="cart[$i][$vehicleName]" value="<?php echo $result['vehicleName']; ?>" />
+                <input type="hidden" name="cart[$sum][$total]" value="<?php echo $result['total']; ?>" />
+
+
               </tr>
               <?php 
                 $sum = $sum+ $total;
               ?>
+
+              <input type="hidden" name="total_amount" value="<?php echo $sum; ?>" />
+              <input type="hidden" name="store_id" value="testbox" />
+              <input type="hidden" name="tran_id" value="58ff22927f82b" />
+              <input type="hidden" name="success_url" value="https://sandbox.sslcommerz.com/developer/success.php" />
+              <input type="hidden" name="fail_url" value="https://sandbox.sslcommerz.com/developer/fail.php" />
+              <input type="hidden" name="cancel_url" value="https://sandbox.sslcommerz.com/developer/cancel.php" />
+              <input type="hidden" name="version" value="3.00" /> 
+
+
               
               <?php } }?>
             </table>
@@ -117,14 +139,32 @@
               </tr>
 
           </table>
+
+            <input type="hidden" name="cus_name" value="<?php echo $result['name']; ?>">
+            <input type="hidden" name="cus_email" value="<?php echo $result['email']; ?>"> 
+            <input type="hidden" name="cus_add1" value="<?php echo $result['address']; ?>">
+              <input type="hidden" name="cus_city" value="<?php echo $result['city']; ?>">
+            <input type="hidden" name="cus_phone" value="<?php echo $result['phone']; ?>">
+
+  <!--  <input type="hidden" name="ship_name" value="<?php echo $result['name']; ?>">
+            <input type="hidden" name="ship_add1" value="<?php echo $result['address']; ?>">
+              <input type="hidden" name="ship_city" value="<?php echo $result['city']; ?>">
+            <input type="hidden" name="ship_phone" value="<?php echo $result['phone']; ?>">
+
+ -->
+          
           <?php } } ?>
         </div>
  		</div>
  	</div>
-  <div class="booking">
-          <a href="?bookingid=booking" title="">Booking Now</a>
+
+     <div class="booking">
+          <input type="submit" name="submit" value="Pay Now" />
         </div>
 	</div>
-   
+
+ 
+</form> 
+
 
 <?php include 'inc/footer.php';?>
